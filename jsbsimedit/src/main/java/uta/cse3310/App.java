394e855690856f3524ddc3be1350b83db902872d
Bud Davis <jimmie.davis@uta.edu>
2025-10-23 20:05:04 -0500

    Added build of jaxb files.  And usage example in App.java.

--- jsbsimedit/src/main/java/uta/cse3310/App.java
+++ jsbsimedit/src/main/java/uta/cse3310/App.java
@@ -4,6 +4,17 @@ package uta.cse3310;
  * Hello world!
  *
  */
+
+
+import java.io.File;
+import java.util.List;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+import jakarta.xml.bind.Marshaller;
+import generated.FdmConfig;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -11,6 +22,43 @@ import java.awt.event.KeyEvent;
 public class App {
 
     public static void main(String[] args) {
+
+	    // start jaxb / xml example code
+   try {
+
+      File file = new File("f16.xml");
+      // JAXBContext jaxbContext = JAXBContext.newInstance(FdmConfig.class);
+
+      // Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+      // FdmConfig cfg = (FdmConfig) jaxbUnmarshaller.unmarshal(file);
+
+      // System.out.println(cfg.getName());
+
+      JAXBContext jc = JAXBContext.newInstance("generated");
+
+      Unmarshaller um = jc.createUnmarshaller();
+      FdmConfig cfg = (FdmConfig) um.unmarshal(file);
+
+      System.out.println(cfg);
+      System.out.println(cfg.getFileheader().getCopyright());
+      System.out.println(cfg.getFileheader().getVersion() );
+      System.out.println(cfg.getAerodynamics().getAxis().get(0).getName());
+      System.out.println(cfg.getAerodynamics().getAxis().get(0).getDocumentationOrFunction());
+      System.out.println(cfg.getAerodynamics().getAxis().get(0).getClass());
+
+
+      //Marshaller m = jc.createMarshaller();
+      //m.setProperty("jaxb.formatted.output", true);
+      //m.marshal(cfg, System.out);
+
+    } catch (JAXBException e) {
+      e.printStackTrace();
+    }
+//
+//
+//
+//  end xml / jaxb example code
+//
         // Create the main frame
         JFrame frame = new JFrame("JTabbedPane Example");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
