ef285bdc15599a6d8f89d01606a554872302afda
mdn2893 <21lqmnguyen7@gmail.com>
2025-11-04 19:44:18 -0600

    Temporarily commented out code that does not compile

--- jsbsimedit/src/main/java/uta/cse3310/commander/view/tabs/MetricsTab.java
+++ jsbsimedit/src/main/java/uta/cse3310/commander/view/tabs/MetricsTab.java
@@ -1,5 +1,5 @@
 package uta.cse3310.commander.view.tabs;
-import javax.swing.*
+import javax.swing.*;
 public class MetricsTab extends BaseTab {
     @Override
     public void initializeTab() {
@@ -11,16 +11,17 @@ public class MetricsTab extends BaseTab {
     String htailarm;
     String vtailarea;
     String vtailarm;   
-
+        /*
         JLabel wingareaLabel = new JLabel("Wingarea: " + wingspan);
         JLabel wingspanLabel = new JLabel("Wingspan: " + wingarea);
         JLabel chordLabel = new JLabel("Chord: " + chord);
-        JLabel htailareaLabel = new JLabel("HTailArea: + htailarea);
-        JLabel htailarmLabel = new JLabel("HTailArm: + htailarm);
-        JLabel vtailareaLabel = new JLabel("VTailArea: + vtailarea);
-        JLabel vtailarmLabel = new JLabel("HTailArea: + vtailarm);
+        JLabel htailareaLabel = new JLabel("HTailArea: " + htailarea);
+        JLabel htailarmLabel = new JLabel("HTailArm: " + htailarm);
+        JLabel vtailareaLabel = new JLabel("VTailArea: " + vtailarea);
+        JLabel vtailarmLabel = new JLabel("HTailArea: " + vtailarm);
+        */
     }
-    
+
     @Override
     public void refreshData() {
         // TODO: Implement metrics data refresh
