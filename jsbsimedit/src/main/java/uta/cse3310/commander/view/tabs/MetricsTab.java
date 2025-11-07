c6ef581652c20827383ebaec274c50a0a99fe49e
Ishan Singh <you@example.com>
2025-11-06 19:49:03 -0600

    Fixed simple syntax errors within MetricsTab

--- jsbsimedit/src/main/java/uta/cse3310/commander/view/tabs/MetricsTab.java
+++ jsbsimedit/src/main/java/uta/cse3310/commander/view/tabs/MetricsTab.java
@@ -1,5 +1,5 @@
 package uta.cse3310.commander.view.tabs;
-import javax.swing.*
+import javax.swing.*;
 public class MetricsTab extends BaseTab {
     @Override
     public void initializeTab() {
@@ -15,10 +15,10 @@ public class MetricsTab extends BaseTab {
         JLabel wingareaLabel = new JLabel("Wingarea: " + wingspan);
         JLabel wingspanLabel = new JLabel("Wingspan: " + wingarea);
         JLabel chordLabel = new JLabel("Chord: " + chord);
-        JLabel htailareaLabel = new JLabel("HTailArea: + htailarea);
-        JLabel htailarmLabel = new JLabel("HTailArm: + htailarm);
-        JLabel vtailareaLabel = new JLabel("VTailArea: + vtailarea);
-        JLabel vtailarmLabel = new JLabel("HTailArea: + vtailarm);
+        JLabel htailareaLabel = new JLabel("HTailArea:  "+ htailarea);
+        JLabel htailarmLabel = new JLabel("HTailArm: "+ htailarm);
+        JLabel vtailareaLabel = new JLabel("VTailArea: "+ vtailarea);
+        JLabel vtailarmLabel = new JLabel("HTailArea: "+ vtailarm);
     }
     
     @Override
