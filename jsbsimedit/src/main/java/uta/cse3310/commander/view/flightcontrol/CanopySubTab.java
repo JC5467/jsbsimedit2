1080433979140a04fb562e01b367535a5440e552
allisonturnbow <abt3885@mavs.uta.edu>
2025-11-09 21:05:09 -0600

    Create CanopySubTab.java

---
+++ jsbsimedit/src/main/java/uta/cse3310/commander/view/flightcontrol/CanopySubTab.java
@@ -0,0 +1,8 @@
+package uta.cse3310.commander.view.flightcontrol;
+
+public class CanopySubTab {
+    // Canopy control subsystem UI
+    public void initializeCanopyControls() {
+        // TODO: Implement canopy control UI
+    }
+}
