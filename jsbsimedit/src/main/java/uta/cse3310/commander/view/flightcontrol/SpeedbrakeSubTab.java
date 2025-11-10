e4b26a5fab162b27975325b0b6c13dbb685aa2c7
allisonturnbow <abt3885@mavs.uta.edu>
2025-11-09 21:02:05 -0600

    Create SpeedbrakeSubTab.java

---
+++ jsbsimedit/src/main/java/uta/cse3310/commander/view/flightcontrol/SpeedbrakeSubTab.java
@@ -0,0 +1,8 @@
+package uta.cse3310.commander.view.flightcontrol;
+
+public class SpeedbrakeSubTab {
+    // Speedbrake control subsystem UI
+    public void initializeSpeedbrakeControls() {
+        // TODO: Implement speedbrake control UI
+    }
+}
