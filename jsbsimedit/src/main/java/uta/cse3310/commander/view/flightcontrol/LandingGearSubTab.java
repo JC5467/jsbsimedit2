30cabd55e62dd4c5ab1523c83c89ca3b5fc6afa8
allisonturnbow <abt3885@mavs.uta.edu>
2025-11-09 21:04:00 -0600

    Create LandingGearSubTab.java

---
+++ jsbsimedit/src/main/java/uta/cse3310/commander/view/flightcontrol/LandingGearSubTab.java
@@ -0,0 +1,8 @@
+package uta.cse3310.commander.view.flightcontrol;
+
+public class LandingGearSubTab {
+    // Landing Gear control subsystem UI
+    public void initializeLandingGearControls() {
+        // TODO: Implement landing gear control UI
+    }
+}
