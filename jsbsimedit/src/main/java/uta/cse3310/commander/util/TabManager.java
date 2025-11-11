b362ea823ee0eaae3a61fa01f6cdb8ebaa710063
Bud Davis <jimmie.davis@uta.edu>
2025-11-11 15:52:23 -0600

    Deleted files that are not needed, or do not seem to be needed, or are in the wrong place in the tree.

--- jsbsimedit/src/main/java/uta/cse3310/commander/util/TabManager.java
+++
@@ -1,8 +0,0 @@
-package uta.cse3310.commander.util;
-
-public class TabManager {
-    // Manages tab navigation and state
-    public void switchToTab(String tabName) {
-        // TODO: Implement tab management
-    }
-}
