79a87580ae319d13efc09b5d8d0955b9066aa1b3
mdn2893 <21lqmnguyen7@gmail.com>
2025-11-11 21:36:47 -0600

    Implemented some of GroundReactionsTab

--- jsbsimedit/src/main/java/uta/cse3310/tab/concreteTabs/GroundReactionsTab.java
+++ jsbsimedit/src/main/java/uta/cse3310/tab/concreteTabs/GroundReactionsTab.java
@@ -1,41 +1,101 @@
 package uta.cse3310.tab.concreteTabs;
 
 import javax.swing.*;
+import java.awt.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.DefaultTableModel;
+
 import uta.cse3310.tab.simpleTab;
 import uta.cse3310.dataStore;
 import uta.cse3310.tabFrame;
 
+import generated.Contact;
+import generated.GroundReactions;
+import generated.Location;
+import uta.cse3310.tab.widgets.textFieldWLabel;
+
 public class GroundReactionsTab extends simpleTab {
     // attributes that only pertain to the 'concrete' tab called oneTab
     // Following UIID094 - UIID098
+    
+        public JTable table;
+        public DefaultTableModel tableModel;
 
     public GroundReactionsTab(tabFrame tf, dataStore ds, String label) {
         super(ds, label);
         System.out.println("in GroundReactions constructor");
 
         TF = tf;
-        // Plan: Implement an editable table tab using JTable
-        // Data will be passed from JAXB parsing of the XML file
-        // TODO: automate the data (by parsing the XML) instead of hardcoding an array
+        panel.setLayout(new BorderLayout());
+        panel.add(new JLabel("No aircraft file read.", SwingConstants.CENTER), BorderLayout.CENTER);
         // TODO: allow the user to edit the XML in the GUI
-
-        String[] columnNames = { "Contact Name", "Contact Type", "Brake Group" };
-        Object[][] data = {
-                { "NOSE_LG", "BOGEY", "NOSE" },
-                { "LEFT_MLG", "BOGEY", "LEFT" },
-                { "RIGHT_MLG", "BOGEY", "RIGHT" }
-        };
-        JTable table = new JTable(data, columnNames);
-        JScrollPane scrollPane = new JScrollPane(table);
-        panel.add(new JTable(data, columnNames));
-        panel.add(scrollPane);
-
-        // what is unique about this tab
-        // panel.setLayout(new FlowLayout()); // Use FlowLayout for panel2
-        // panel.add(new JLabel("Enter your name:"));
-        // panel.add(new JTextField(15));
-        // panel.setBackground(new Color(255, 220, 200)); // Light orange background
+        // TODO: implement DETAIL WINDOW
+        // TODO: DO NOT ALLOW USER TO EDIT TABLE FROM TAB, ONLY DETAIL WINDOW
 
     }
+
+    //data fields to be included:
+    //C.name, C.type, Location.coords, Stat-fcoef, dyam-fcoef, rolling, spring, damping, steer, brake group, retractable
+
+    public void loadData() {
+
+        panel.removeAll();
+        //Initialize table when data is loaded
+        String[] columnNames = {"Contact Name", "Location (X,Y,Z)", "UNIT", "Brake Group"};
+        tableModel = new DefaultTableModel(columnNames, 0);
+        table = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(table);
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        //Buttons
+        JPanel buttonPanel = new JPanel();
+        JButton addButton = new JButton("Add");
+        JButton deleteButton = new JButton("Delete");
+        JButton detailButton = new JButton("Detail");
+        buttonPanel.add(addButton);
+        buttonPanel.add(deleteButton);
+        buttonPanel.add(detailButton);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+
+        // Button actions
+        addButton.addActionListener(e -> {
+        tableModel.addRow(new Object[]{"new_contact", "", "", ""});
+        });
+
+        deleteButton.addActionListener(e -> {
+        int selected = table.getSelectedRow();
+        if (selected != -1){tableModel.removeRow(selected);}
+        });
+
+        for (Object obj : DS.cfg.getGroundReactions().getContent()) {
+            if (obj instanceof Contact) {
+                Contact contact = (Contact) obj;
+                String name = contact.getName();
+
+                Location location = contact.getLocation();
+                String unit = "IN";
+                String locX = "0", locY = "0", locZ = "0";
+                String locCombined = "0";
+                if (location != null) {
+                    unit = location.getUnit() != null ? location.getUnit().value() : "IN";
+                    locX = String.valueOf(location.getX());
+                    locY = String.valueOf(location.getY());
+                    locZ = String.valueOf(location.getZ());
+                    locCombined = String.format("%s, %s, %s", locX, locY, locZ);
+
+                }
+
+                String bGroup = contact.getBrakeGroup() != null ? contact.getBrakeGroup() : "NONE";
+
+                tableModel.addRow(new Object[]{name, locCombined, unit, bGroup});
+            }
+        }
+
+    }
+    
+
+
+
 
 }
