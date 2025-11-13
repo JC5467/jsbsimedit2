878fcfadcdf2780fd516c3837234fcbdc68257ce
allisonturnbow <abt3885@mavs.uta.edu>
2025-11-12 18:40:01 -0600

    Start on reading the xml file into flightcontrol

---
+++ jsbsimedit/src/main/java/uta/cse3310/commander/parser/FlightControlXMLLoader.java
@@ -0,0 +1,12 @@
+package uta.cse3310.commander.parser;
+
+import generated.FdmConfig;
+import generated.Channel;
+
+import uta.cse3310.commander.model.FlightControlModel;
+
+public class FlightControlXMLLoader {
+
+    
+    
+}
