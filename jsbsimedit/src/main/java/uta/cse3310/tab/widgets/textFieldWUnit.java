56d8726a61d4117ceb75184dd4ce39dcdb26e141
chr15rod20 <cxr7522@mavs.uta.edu>
2025-11-19 13:46:32 -0600

    further implementation on line reduction in metrics tab

--- jsbsimedit/src/main/java/uta/cse3310/tab/widgets/textFieldWUnit.java
+++ jsbsimedit/src/main/java/uta/cse3310/tab/widgets/textFieldWUnit.java
@@ -6,6 +6,7 @@ import uta.cse3310.tab.widgets.setValFunction;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import generated.Metrics;
 import generated.AreaUnit;
 import generated.LengthUnit;
 
@@ -13,39 +14,45 @@ public class textFieldWUnit {
 	private JLabel label;
 	private JLabel jl;
 
-        public textFieldWUnit(JPanel panel, String labelText, Integer labelX, Integer labelY,
+        public textFieldWUnit(JPanel panel, dirtyFunction DF, Metrics.Location loc, String labelText, Integer labelX, Integer labelY,
     		Integer labelW, Integer labelH,String unitS,Integer unitX, Integer unitY, Integer unitW,
                 Integer unitH, String unit, Integer boxX, Integer boxY, Integer boxW,
                 Integer boxH){
-
+        
                 System.out.println("in textFieldWUnit widget constructor");
-	
+                        
                 JComboBox unitBoxLen = new JComboBox<>(LengthUnit.values());
                 JComboBox unitBoxArea = new JComboBox<>(AreaUnit.values());
                 String search = "area";
-
+                        
                 label = new JLabel(labelText);
                 label.setBounds(labelX, labelY, labelW, labelH);
                 panel.add(label);
-
+                        
                 jl = new JLabel(unitS);
                 jl.setBounds(unitX,unitY,unitW,unitH);
-                panel.add(jl);
-                
+                        panel.add(jl);
+                        
                 if(labelText.toLowerCase().indexOf(search.toLowerCase()) != -1){
                         AreaUnit AU = AreaUnit.valueOf(unit);
                         unitBoxArea.setSelectedItem(AU);
                         unitBoxArea.setBounds(boxX,boxY,boxW,boxH);
                         panel.add(unitBoxArea);
+                        unitBoxArea.addActionListener(e -> {
+                        loc.setUnit((LengthUnit) unitBoxArea.getSelectedItem());
+                        DF.set();
+                        });
                 }
                 else{
                         LengthUnit LU = LengthUnit.valueOf(unit);
                         unitBoxLen.setSelectedItem(LU);
                         unitBoxLen.setBounds(boxX,boxY,boxW,boxH);
                         panel.add(unitBoxLen);
-                }
-
-                
+                        unitBoxLen.addActionListener(e -> {
+                        loc.setUnit((LengthUnit) unitBoxLen.getSelectedItem());
+                        DF.set();
+                        });
+                }       
 		
     }
 }
